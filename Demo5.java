bc0480cf9472be4ba3ed4a502e678a51704a264f
RavinduNKK <ravindunkkudagama@gmail.com>
2023-07-31 10:14:39 +0530

    fifth task is done

---
+++ Demo5.java
@@ -0,0 +1,10 @@
+public class Demo5 {
+    public static void main(String[] args) {
+        int block1 = 978;
+        int block2 = 3;
+        int block3 = 16;
+        int block4 = 1484100;
+
+        System.out.printf("\033[1;31m%d\033[0m-\033[1;32m%d\033[0m-\033[1;34m%d\033[0m-\033[1;36m%d\033[0m\n", block1, block2, block3, block4);
+    }
+}
