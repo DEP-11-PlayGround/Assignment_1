b8dc3e2fca00f9c8b9f32d204e4f874c48ca504d
RavinduNKK <ravindunkkudagama@gmail.com>
2023-07-31 11:21:50 +0530

    six task a done

---
+++ Demo6.java
@@ -0,0 +1,8 @@
+public class Demo6 {
+    public static void main(String[] args) {
+        System.out.printf("\u001B[41m[   5\u001B[42m0%%  \u001B[42m]\u001B[0m\n");
+        System.out.print(" ");
+    }
+}
+
+
