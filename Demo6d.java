c500a78dabc6de7811924ce84e87cf091391839b
RavinduNKK <ravindunkkudagama@gmail.com>
2023-07-31 14:40:18 +0530

    six task d price  colour updated

--- Demo6d.java
+++ Demo6d.java
@@ -6,8 +6,8 @@ public class Demo6d {
         System.out.printf("+------------+------------+-------------+%n");
 
        
-        System.out.printf("| %-10s | \u001B[34m%10d\u001B[0m | $%-10.2f |%n", "Apples", 5, 0.99);
-        System.out.printf("| %-10s | \u001B[34m%10d\u001B[0m | $%-10.2f |%n", "Oranges", 10, 1.49);
+        System.out.printf("| %-10s | \u001B[34m%10d\u001B[0m | \u001B[32m$\u001B[0m\u001B[31m%-10.2f\u001B[0m |%n", "Apples", 5, 0.99);
+        System.out.printf("| %-10s | \u001B[34m%10d\u001B[0m | \u001B[32m$\u001B[0m\u001B[31m%-10.2f\u001B[0m |%n", "Oranges", 10, 1.49);
         System.out.printf("+------------+------------+-------------+%n");
     }
 }
