5d1fa4d27f840443cbdc208d35bc3aa863224a74
RavinduNKK <ravindunkkudagama@gmail.com>
2023-07-31 14:38:20 +0530

    six task d quantity colour updated

--- Demo6d.java
+++ Demo6d.java
@@ -6,8 +6,8 @@ public class Demo6d {
         System.out.printf("+------------+------------+-------------+%n");
 
        
-        System.out.printf("| %-10s | %10d | $%-10.2f |%n", "Apples", 5, 0.99);
-        System.out.printf("| %-10s | %10d | $%-10.2f |%n", "Oranges", 10, 1.49);
+        System.out.printf("| %-10s | \u001B[34m%10d\u001B[0m | $%-10.2f |%n", "Apples", 5, 0.99);
+        System.out.printf("| %-10s | \u001B[34m%10d\u001B[0m | $%-10.2f |%n", "Oranges", 10, 1.49);
         System.out.printf("+------------+------------+-------------+%n");
     }
 }
