6cdf5deffdbe65a3fa7025971b800377ff93c518
RavinduNKK <ravindunkkudagama@gmail.com>
2023-07-31 13:26:36 +0530

    six task b is done

---
+++ Demo6b.java
@@ -0,0 +1,15 @@
+public class Demo6b {
+    public static void main(String[] args) {
+        
+        System.out.printf("+-----------------------+----------------+\n");
+        System.out.printf("| Name                  | Age            |\n");
+        System.out.printf("+-----------------------+----------------+\n");
+
+        
+        System.out.printf("| Alice                 | 24             |\n");
+        System.out.printf("| Bob                   | 30             |\n");
+
+        System.out.printf("+-----------------------+----------------+\n");
+    }
+}
+
